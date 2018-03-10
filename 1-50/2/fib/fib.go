9a88d3b5f42afa0762670af408c306ec11eb13a4
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-10 00:19:03 -0700

    Add ability to find the n-th Fibonacci number.

---
+++ 1-50/2/fib/fib.go
@@ -0,0 +1,12 @@
+package fib
+
+// Fib recursively calculates the n-th fibonacci digit
+func Fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return Fib(n-1) + Fib(n-2)
+	}
+}
