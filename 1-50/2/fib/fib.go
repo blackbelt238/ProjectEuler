bc16046f12f15b82175270ef001dc12393ec9082
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-10 00:58:10 -0700

    Add ability to get all Fibonacci numbers whose value does not exceed a given limit.

--- 1-50/2/fib/fib.go
+++ 1-50/2/fib/fib.go
@@ -1,12 +1,29 @@
 package fib
 
-// Fib recursively calculates the n-th fibonacci digit
+// Fib calculates the n-th Fibonacci digit
 func Fib(n int) int {
-	if n == 0 {
+	if n == 0 { // n = 0
 		return 0
-	} else if n == 1 {
+	} else if n == 1 { // n = 1
 		return 1
 	} else {
-		return Fib(n-1) + Fib(n-2)
+		return Fib(n-1) + Fib(n-2) // Fn = Fn-1 + Fn-2
 	}
+}
+
+// UpToLimit returns a list of all Fibonacci numbers whose sum does not exceed limit
+func UpToLimit(limit int) []int {
+	fibl := []int{0, 1} // list of Fibonacci numbers
+	sum := 1            // sum of all numbers in fibl
+
+	var next int
+	for sum <= limit {
+		next = fibl[len(fibl)-1] + fibl[len(fibl)-2] // calculate the next number to add
+		fibl = append(fibl, next)                    // add next to the list
+		sum += next                                  // update the sum
+	}
+
+	// return the constructed list without the final entry,
+	//   since it caused the sum to exceed the limit
+	return fibl[:len(fibl)-2]
 }
