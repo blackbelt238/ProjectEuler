e31f3b1f21c4d2de497d1a354337868ff085a3af
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-10 00:58:33 -0700

    Add test for UpToLimit.

--- 1-50/2/fib/fib_test.go
+++ 1-50/2/fib/fib_test.go
@@ -11,3 +11,29 @@ func TestFib(t *testing.T) {
 		t.Errorf("The 12th Fibonacci number should be 144, instead got %d", num)
 	}
 }
+
+// TestUpToLimit ensures UpToLimit is returning the correct slice of Fibonacci numbers
+func TestUpToLimit(t *testing.T) {
+	fcor := []int{0, 1, 1, 2, 3, 5} // the desired slice
+	futl := UpToLimit(12)           // slice recieved from UptoLimit
+
+	for i := 0; i < len(futl); i++ {
+		if fcor[i] != futl[i] {
+			t.Error("Error retrieving entries whose sum does not exceed 12.\n")
+			t.Errorf("\tcorrect entries: %v", fcor)
+			t.Errorf("\tcorrect entries: %v", futl)
+		}
+	}
+
+	// test the smallest case
+	fcor = []int{0}
+	futl = UpToLimit(0)
+
+	for i := 0; i < len(futl); i++ {
+		if fcor[i] != futl[i] {
+			t.Error("Error retrieving entries whose sum does not exceed 12.\n")
+			t.Errorf("\tcorrect entries: %v", fcor)
+			t.Errorf("\tcorrect entries: %v", futl)
+		}
+	}
+}
