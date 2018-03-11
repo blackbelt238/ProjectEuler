d0d5154f1425dd918e6f6ad7d46f630e3b6b7bba
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-10 22:45:46 -0700

    Minor code clean-ups.

--- 1-50/3/primefactor/primefactor.go
+++ 1-50/3/primefactor/primefactor.go
@@ -6,10 +6,11 @@ import (
 
 // PrimeFactors returns all the prime factors for the given number
 func PrimeFactors(num int) []int {
-	primes := SieveOfEratosthenes(int(math.Floor(math.Sqrt(float64(num))))) // all prime numbers up to floor(sqrt(num))
-	var pfacs []int                                                         // prime factors for num
+	primes := SieveOfEratosthenes(int(math.Sqrt(float64(num)))) // all prime numbers up to sqrt(num)
+	var pfacs []int                                             // prime factors for num
 
 	for _, prime := range primes {
+		// if the prime is a prime factor
 		if num%prime == 0 {
 			pfacs = append(pfacs, prime)
 		}
@@ -22,23 +23,21 @@ func PrimeFactors(num int) []int {
 func SieveOfEratosthenes(n int) []int {
 	// create the sieve, initialize all values to true
 	sieve := make([]bool, n+1)
-	for i := range sieve {
-		sieve[i] = true
-	}
 	var primes []int
 
 	// for each prime in the sieve
-	for num := 2; num <= int(math.Floor(math.Sqrt(float64(n)))); num++ {
-		if sieve[num] == true {
-			// mark off its' multiple
-			for mult := int(math.Floor(math.Pow(float64(num), 2))); mult <= n; mult += num {
-				sieve[mult] = false
+	for num := 2; num <= int(math.Sqrt(float64(n))); num++ {
+		if sieve[num] == false {
+			// mark off its' multiple as composite
+			for mult := num * num; mult <= n; mult += num {
+				sieve[mult] = true
 			}
 		}
 	}
 
-	for num, isPrime := range sieve {
-		if isPrime {
+	// for each number that is not composite, add it to the list of primes
+	for num, isComposite := range sieve {
+		if !isComposite {
 			primes = append(primes, num)
 		}
 	}
