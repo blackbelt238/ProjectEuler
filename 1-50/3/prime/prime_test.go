65086b8375cbfd902abc7d8bcb3f6ca2e1004637
Ethan Peterson <cap2010.ep@gmail.com>
2018-03-21 13:37:59 -0600

    Tests for determining primeness and finding n-th prime.

--- 1-50/3/prime/prime_test.go
+++ 1-50/3/prime/prime_test.go
@@ -38,6 +38,53 @@ func TestFactors(t *testing.T) {
 	}
 }
 
+// TestGetPrime ensures that GetPrime can find the n-th prime
+func TestGetPrime(t *testing.T) {
+	six := GetPrime(6)
+	if six != 13 {
+		t.Errorf("The sixth prime is 13, but instead got %d", six)
+	}
+
+	twenty5 := GetPrime(25)
+	if twenty5 != 97 {
+		t.Errorf("The 25th prime is 97, but instead got %d", twenty5)
+	}
+
+	cincociento := GetPrime(500)
+	if cincociento != 3571 {
+		t.Errorf("The 500th prime is 3581, but instead got %d", cincociento)
+	}
+}
+
+func TestIsPrime(t *testing.T) {
+	var num int
+
+	num = 10
+	if IsPrime(num) {
+		t.Errorf("%d was found to be prime, but isnt", num)
+	}
+
+	num = 13 // 6th prime
+	if !IsPrime(num) {
+		t.Errorf("%d was found to be not prime, but is", num)
+	}
+
+	num = 97 // 25th prime
+	if !IsPrime(num) {
+		t.Errorf("%d was found to be not prime, but is", num)
+	}
+
+	num = 3580
+	if IsPrime(num) {
+		t.Errorf("%d was found to be prime, but isn't", num)
+	}
+
+	num = 3581 // 501st prime
+	if !IsPrime(num) {
+		t.Errorf("%d was found to be not prime, but is", num)
+	}
+}
+
 // TestSieveOfEratosthenes ensures SieveOfEratosthenes is returning the expected list of primes
 func TestSieveOfEratosthenes(t *testing.T) {
 	pexp := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29} // the expected list of primes
