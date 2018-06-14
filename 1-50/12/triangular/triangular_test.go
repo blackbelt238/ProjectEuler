e2758b44cf49349f0005eae35120cf639c52b833
Ethan Peterson <cap2010.ep@gmail.com>
2018-06-13 19:58:55 -0600

    Finding a triangular must work in both directions.

--- 1-50/12/triangular/triangular_test.go
+++ 1-50/12/triangular/triangular_test.go
@@ -10,4 +10,9 @@ func TestFind(t *testing.T) {
 	if f != 28 {
 		t.Errorf("finding the 7th triangular number yielded %d, should be 28", f)
 	}
+
+	f = tri.Find(3)
+	if f != 6 {
+		t.Errorf("finding the 3rd triangular number yielded %d, should be 6", f)
+	}
 }
