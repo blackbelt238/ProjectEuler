b9cb4fa45570b1924e5eac7e4a6d61f9e1195abe
Ethan Peterson <cap2010.ep@gmail.com>
2018-06-17 21:45:07 -0600

    Test 0s in multiplier.

--- 1-50/13/longmath/longmath_test.go
+++ 1-50/13/longmath/longmath_test.go
@@ -66,6 +66,14 @@ func TestLongMult(t *testing.T) {
 		t.Errorf("incorrect sum of %s and %s calculated: got %s, expected %s", n1, n2, res, exp)
 	}
 
+	n1, n2 = "10", "20"
+	res = LongMult(n1, n2)
+	exp = "200"
+
+	if res != exp {
+		t.Errorf("incorrect sum of %s and %s calculated: got %s, expected %s", n1, n2, res, exp)
+	}
+
 	n1, n2 = "612", "24"
 	res = LongMult(n1, n2)
 	exp = "14688"
@@ -77,6 +85,14 @@ func TestLongMult(t *testing.T) {
 	n1, n2 = "423", "211"
 	res = LongMult(n1, n2)
 	exp = "89253"
+
+	if res != exp {
+		t.Errorf("incorrect sum of %s and %s calculated: got %s, expected %s", n1, n2, res, exp)
+	}
+
+	n1, n2 = "674", "220002"
+	res = LongMult(n1, n2)
+	exp = "148281348"
 
 	if res != exp {
 		t.Errorf("incorrect sum of %s and %s calculated: got %s, expected %s", n1, n2, res, exp)
