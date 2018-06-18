48fa57c7c348bd47c50d0db86a123b6b87c61d13
Ethan Peterson <cap2010.ep@gmail.com>
2018-06-17 21:56:59 -0600

    Add ability to long multiply two stringified numbers.

--- 1-50/13/longmath/longmath.go
+++ 1-50/13/longmath/longmath.go
@@ -45,6 +45,66 @@ func longAddDigit(d1, d2, d3 byte) (byte, byte) {
 	return sum[0], carry
 }
 
+// LongMult performs long multiplication on the numbers given in string form
+func LongMult(n1, n2 string) string {
+	res := "0"         // overall result of long multiplication
+	var prod byte      // intermediate product of digits
+	carry := byte('0') // carried digit resulting from a sum of digits
+
+	var partials [][]byte // all partial products to sum
+
+	cand, lier := []byte(n1), []byte(n2)
+	// ensure the longer number is the multiplicand
+	if len(lier) > len(cand) {
+		cand, lier = lier, cand
+	}
+
+	for i := len(lier) - 1; i >= 0; i-- {
+		// if the multiplier's current digit is 0, don't generate a partial
+		if lier[i] == byte('0') {
+			continue
+		}
+
+		partial := make([]byte, 0, len(cand)) // the partial product for this digit
+		// align the partial with the corresponding multiplier digit
+		for ralign := (len(lier) - 1) - i; ralign > 0; ralign-- {
+			partial = append(partial, byte('0'))
+		}
+
+		// multiply the multiplier's current digit with each digit of the multiplicand
+		for j := len(cand) - 1; j >= 0; j-- {
+			prod, carry = longMultDigit(lier[i], cand[j], carry)
+			partial = append([]byte{prod}, partial...)
+		}
+		partials = append(partials, partial) // save the partial product for summing
+	}
+
+	// sum all partial products
+	for _, partial := range partials {
+		res = LongAdd(res, string(partial))
+	}
+
+	return res
+}
+
+// longMultDigit performs long multiplication between 2 primary digits and adds a carry
+func longMultDigit(d1, d2, d3 byte) (byte, byte) {
+	// perform addition on the arguments and
+	n1, _ := strconv.Atoi(string(d1))
+	n2, _ := strconv.Atoi(string(d2))
+	n3, _ := strconv.Atoi(string(d3)) // the carry
+	prod := strconv.Itoa(n1*n2 + n3)
+
+	// determine the value of the carry
+	carry := byte('0')
+	if len(prod) > 1 {
+		carry = prod[0]
+		prod = prod[1:]
+	}
+
+	return prod[0], carry
+}
+
 // paddedBytes returns the stringified numbers as byte arrays,
 //   with the shorter one front-padded to match lengths
 func paddedBytes(n1, n2 string) ([]byte, []byte) {
