39418872acf2e99157c2955c2b334d543885854a
Ethan Peterson <cap2010.ep@gmail.com>
2018-06-18 22:39:00 -0600

    Test counting the letters in a number.

--- 1-50/17/numwords/numwords_test.go
+++ 1-50/17/numwords/numwords_test.go
@@ -4,6 +4,29 @@ import (
 	"testing"
 )
 
+func TestLetters(t *testing.T) {
+	var word string
+	var count, exp int
+
+	word = Word(1)
+	count = Letters(word)
+	exp = 3
+	if count != exp {
+		t.Errorf("%s has %d letters, but got %d", word, exp, count)
+	}
+
+	word += Word(2)
+	word += Word(3)
+	word += Word(4)
+	word += Word(5)
+	count = Letters(word)
+
+	exp = 19
+	if count != exp {
+		t.Errorf("%s has %d letters, but got %d", word, exp, count)
+	}
+}
+
 func TestWord(t *testing.T) {
 	var num int
 	var word, exp string
